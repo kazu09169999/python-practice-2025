c6e518e68f7d21b2cbbd75da07011e36a1e164e7
kazu09169999 <kazuki.yoneyama.78@dl-techx.com>
2025-08-07 18:04:23 +0900

    change

--- week1/hello.py
+++ week1/hello.py
@@ -1,1 +1,8 @@
-print("Hello, python practice.")
+name = "kazuki"
+hometown = "tokyo"
+hobby = "baseball"
+
+print("Hi")
+print(f"my name is {name}.")
+print(f"I am from {hometown}.")
+print(f"my hobby is {hobby}.")
