ff5103f361e9358316f9b0894c2f3788187609ea
kazuki <y.kazu_0916@icloud.com>
2025-08-12 16:05:23 +0900

    completed

--- week1/even_checker.py
+++ week1/even_checker.py
@@ -1,1 +1,9 @@
-print("hello even")
+def is_even_or_odd(number):
+    if number % 2 == 0:
+        print(f"{number} is even")
+    else:
+        print(f"{number} is odd")
+
+# 関数を呼び出す
+number = int(input("enter any numbers: "))
+is_even_or_odd(number)
