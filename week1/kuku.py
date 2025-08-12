eefd82d2863885ed713ccd40d22b1dab5ed55439
kazu09169999 <kazuki.yoneyama.78@dl-techx.com>
2025-08-12 18:07:26 +0900

    completed

--- week1/kuku.py
+++ week1/kuku.py
@@ -2,5 +2,6 @@ print("hello kuku")
 
 for row in range(1, 10):
     for num in range(1, 10):
-        print(f"{row} * {num} = {row * num}", end="\t")
+        print(f"{row} * {num} = {row * num}")
+
     print()  # ← 1行分終わったら改行
