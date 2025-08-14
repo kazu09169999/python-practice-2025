79a0f2cb2ca8a4b2cd71f35138c6c952f522e6c6
kazuki <y.kazu_0916@icloud.com>
2025-08-14 15:03:50 +0900

    complete

--- week1/calculator.py
+++ week1/calculator.py
@@ -1,1 +1,13 @@
-print("hello calculator")
+num1 = float(input("enter number 1: "))
+num2 = float(input("enter number 2: "))
+
+op = input("choose + or - : ")
+
+if op == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif op == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+else:
+    print("error")    
